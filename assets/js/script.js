fa7cb4d48f4eafbb288b878c1b6dc10ac427ba7d
JD Rowley <jdrowley1989@gmail.com>
2021-08-26 16:11:38 -0600

    save city search to local storage and create button elements for them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,73 @@
+var apiKey = "&appid=09dc3595df08a6cbcf8463276de45c90";
+
 var searchFormEl = document.querySelector("#search-form");
-var cityInputEl = document.querySelector("#input-city");
+var cityInputEl = document.querySelector("#city");
+var searchHistoryEl = document.querySelector("#search-history")
+
+var city = cityInputEl.value.trim();
 
 var getWeatherInfo = function() {
-    var response = fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={09dc3595df08a6cbcf8463276de45c90}");
-        console.log(response);
+    // format the one call api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?" + city + "&exclude=minutely,alerts&units=imperial" + apiKey;
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
 getWeatherInfo();
 
-// // create submit handler for search button
-// var formSubmitHandler = function(event) {
-//     event.preventDefault();
+// create submit handler for search button
+var formSubmitHandler = function(event) {
+    event.preventDefault();
 
-//     var cityValue = cityInputEl.value.trim();
+    // create variable for city value
+    var cityValue = cityInputEl.value.trim();
 
-//     if (cityValue) {
-//         cityInputEl.value = "";
-//     } else {
-//         alert("Please enter a city name");
-//     }
-// };
+    console.log(cityValue);
 
-// searchFormEl.addEventListener("click", formSubmitHandler);
+    // // convert city name to coordinates
+    // var getCoordinates = function() {
+    //     var geoApi = "https://nominatim.openstreetmap.org/search?city=" + cityValue + "&format=json";
+
+    //     fetch(geoApi).then(function(response) {
+    //         response.json().then(function(data) {
+    //             console.log(data);
+
+    //         });
+    //     });
+    // };
+
+    // getCoordinates(cityValue);
+    
+    // pass city value into getWeatherInfo function
+    if (cityValue) {
+        getWeatherInfo(cityValue);
+        // clear out the search box
+        cityInputEl.value = "";
+    } else {
+        // alert the user if there is no value for city
+        alert("Please enter a city name");
+        return;
+    }
+
+    var saveCityInput = function() {
+        // save city input into local storage
+        localStorage.setItem("city", cityValue);
+
+        // create a button element with recent searches
+        var recentSearchesEl = document.createElement("button");
+        recentSearchesEl.className = "search-history-btn btn";
+        recentSearchesEl.innerText = cityValue;
+
+        // append the button to search history
+        searchHistoryEl.appendChild(recentSearchesEl);
+    };
+    
+    saveCityInput();
+};
+
+searchFormEl.addEventListener("submit", formSubmitHandler);
